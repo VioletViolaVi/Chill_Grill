44ef2f030532b78b9da14ae00160b3271495702c
VioletViola <vivian.v18723@gmail.com>
2021-04-01 06:01:38 +0100

    Add and style back to top button.

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,20 @@ $(document).ready(function () {
   $(".fas.fa-bars").on("click", () => {
     $("#sideNav").slideToggle("slow");
   });
+
+  // scrolls to top of screen using arrow
+  $(".back-to-top-link").click(function () {
+    window.scrollTo({
+      top: 0,
+    });
+  });
+
+  // shows and hides back to top arrow
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".fixed-arrow").show();
+    } else {
+      $(".fixed-arrow").hide();
+    }
+  });
 });
